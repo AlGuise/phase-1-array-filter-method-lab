23ddc1735db05023b5560572da67f6e90793d844
Alain Giessel <alain.giessel@gmail.com>
2022-02-26 12:48:02 -0700

    commit tree 53cef491aec0fd8aabf655a1bba810cb9803324f

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution here
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+// function findMatching(obj) {
+//     let win = obj.filter((result) => result.drivers === 'Bobby')
+//     if (win) {
+//         return win
+//      }
+// }
+
+
+// let findMatching = [];
+
+// for (let i = 0; i < drivers.length; i++) {
+//     if(drivers == 'Bobby')
+//     driver.push(drivers[i])
+// };
+
+// console.log(findMatching);
+
+
+const findMatching = nameing.filter(function() {
+    if(nameing == 'Bobby') {
+        return true;
+    }
+});
+
+
+const findName = namesFound.filter(findMatching => findMatching === 'Bobby');
+
+console.log(findName);
